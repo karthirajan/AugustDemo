b4964fdee47f38582016658ee0dd0d39a0a9c230
karthirajan <karthick@centricsoftware.com>
2021-08-12 20:25:09 +0530

    Resolved errors

--- src/org/day1/A.java
+++ src/org/day1/A.java
@@ -32,23 +32,7 @@ public final class A {
 	public void tester2() {
 	//you are getting 25 mobile names from the application
 		
-		List<> mobileNames = driver.findElements(By.xpath(""));
 		
-		Set<Webelement>  s = new Hashset<>();
-		s.addAll()
-		
-		int j = mobileNames.size();
-		
-		for (int i = 1; i <= j; i++) {
-			
-			String name = mobileNames.getText();
-			if(name.equals("")){
-				
-			}else{
-				
-			}
-			
-		}
 
 	}
 
