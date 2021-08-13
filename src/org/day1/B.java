1acb043af0cf45b685b0b88257f17873cd4c1e2e
premgthala01 <premgthala@gmail.com>
2021-08-13 17:12:00 +0530

    message

--- src/org/day1/B.java
+++ src/org/day1/B.java
@@ -1,14 +1,16 @@
 package org.day1;
 
+
 public class B {
 	
-	
-	
-	public static void main(String[] args) {
-		
-		
-		A.tester1();
-		
+	public  void firstname() {
+		System.out.println("prem");
 	}
-
+	public  void age() {
+		System.out.println("21");
+	}
+	public static void main(String[] args) {
+		B b=new B();
+		b.firstname();
+	}
 }
