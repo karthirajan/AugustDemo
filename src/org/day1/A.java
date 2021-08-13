621bd6b020f450bfcf7cd946156961f89c9f5d97
premgthala01 <premgthala@gmail.com>
2021-08-13 17:42:15 +0530

    message

--- src/org/day1/A.java
+++ src/org/day1/A.java
@@ -1,17 +1,12 @@
 package org.day1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public final class A {
 	
 	//Access Specifiers 
 	//same package and different package
 	//Inheritance   
 	//Abstraction
-	//polymorphism
-	//encapsulation
-	//datatypes
+	
 	//scanner  - methods and return types
 	//static and final
 	//constructor
@@ -19,21 +14,17 @@ public final class A {
 	//String -- methods -- return types
 	//array
 	//Collection  -- methods -- return types
-	//Exception
-	
-	public final static int i = 0;
-	
-	public static void tester1() {
-	
-		System.err.println("error :"+i);
-
+	public  void firstname() {
+		System.out.println("prem");
 	}
-	
-	public void tester2() {
-	//you are getting 25 mobile names from the application
-		
-		
-
+	public  void age() {
+		System.out.println("21");
 	}
-
+	public static void main(String[] args) {
+		A b=new A();
+		b.firstname();	
+	}
 }
+
+
+
